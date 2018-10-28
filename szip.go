79e97edc3cf73d01bc94c9a2a46217aaf2805c40
TOL00773 <tol00773@gmail.com>
2018-10-28 14:58:22 +0300

    add Application mode "z"

--- szip.go
+++ szip.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"runtime/debug"
 )
 
 func main() {
+	var mode string
 	var err error
+	flag.StringVar(&mode, "mode", "z", "Application mode")
+
+	switch mode {
+	case "z":
+		err = makeSzip()
+	}
 
 	if err != nil {
 		fmt.Printf("Error! Crazy copy-paste leads to injury", err, debug.Stack())
 	}
+}
+
+func makeSzip() (err error)  {
+	fmt.Println("It's makeZip ")
+	return
 }
