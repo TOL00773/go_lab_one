a5e701dee54ccdf89be19d40821c7925aa362779
TOL00773 <tol00773@gmail.com>
2018-10-28 14:54:27 +0300

    add error handling

--- szip.go
+++ szip.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime/debug"
+)
 
 func main() {
-	fmt.Println("three.., two.., one.., GO!!!")
+	var err error
+
+	if err != nil {
+		fmt.Printf("Error! Crazy copy-paste leads to injury", err, debug.Stack())
+	}
 }
